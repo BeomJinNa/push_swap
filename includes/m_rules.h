6c647d633e7b92e89d4b635be9991611cbe301a7
Beomjin Na <bena@c10r6s14.42seoul.kr>
2023-03-16 14:51:04 +0900

    DOCU: Add m_rules.h

---
+++ includes/m_rules.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   m_rules.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 14:29:35 by bena              #+#    #+#             */
+/*   Updated: 2023/03/16 14:42:37 by bena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef M_RULES_H
+# define M_RULES_H
+
+# define SA 101
+# define SB 102
+# define SS 103
+# define PA	104
+# define PB 105
+# define RA 106
+# define RB 107
+# define RR 108
+# define RRA 109
+# define RRB 110
+# define RRR 111
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 12:43:45 by bena              #+#    #+#             */
-/*   Updated: 2023/03/16 13:09:10 by bena             ###   ########.fr       */
+/*   Updated: 2023/03/16 13:26:51 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 #include "s_array.h"
 
 void		initialize_data(t_array *array, int ac, char **av);
+char		*solve_push_swap(t_array *array);
+//void		print_answer(char *answer_sheet);
 static int	terminate_current_program(int output);
 
 int	main(int ac, char **av)
 {
 	t_array	array;
+	char	*answer_sheet;
 
 	if (ac == 1)
 		return (terminate_current_program(0));
@@ -27,6 +30,14 @@ int	main(int ac, char **av)
 	initialize_data(&array, ac, av);
 	if (array.array == NULL)
 		return (terminate_current_program(0));
+	answer_sheet = solve_push_swap(&array);
+	if (answer_sheet == NULL)
+	{
+		free(array.array);
+		return (terminate_current_program(0));
+	}
+	//print_answer(answer_sheet);
+	free(answer_sheet);
 	free(array.array);
 	return (0);
 }
